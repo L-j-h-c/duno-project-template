b0f4d9ab3d8fced88e8231d8657f317bd00b02fb
L-j-h-c <ckrgkswnsgh@naver.com>
2022-04-07 17:02:19 +0900

    [Add] #1 - SampleProject 생성
    SampleProject 생성

---
+++ BasicTemplate/SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Junho Lee on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
